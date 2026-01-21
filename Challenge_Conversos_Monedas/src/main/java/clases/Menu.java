d0cad89d9b4b20328ba0faf95468d95223ee3afe
Roberto-Aguirre <ivanaguirre02@hotmail.com>
2026-01-20 23:29:35 -0600

    Reto completado

---
+++ Challenge_Conversos_Monedas/src/main/java/clases/Menu.java
@@ -0,0 +1,31 @@
+package clases;
+
+public class Menu {
+    public void Welcome ()
+    {
+        System.out.println("Bienvenido al change exchange de Alura Latam");
+    };
+
+    public void MenuOptions ()
+    {
+        System.out.println("1) Dolar -> Peso Mexicano");
+        System.out.println("2) Peso Mexicano -> Dolar");
+        System.out.println("3) Dolar -> Real brasilenyo");
+        System.out.println("4) Real brasilenyo -> Dolar");
+        System.out.println("5) Dolar -> Peso colombiano");
+        System.out.println("6) Peso colombiano -> Dolar");
+        System.out.println("7) Salir");
+
+    };
+
+    public void ChooseOption ()
+    {
+        System.out.print("Eliga una opcion valida: ");
+    };
+
+    public void IngressValue ()
+    {
+        System.out.print("Ingresa un monto a convertir: ");
+    };
+
+}
